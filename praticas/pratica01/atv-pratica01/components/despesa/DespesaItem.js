160bafe1243d65ecadcf8402ef76e9836e0c6b3a
Rafael Ramos <ottoni1504@gmail.com>
2026-04-01 23:57:31 -0300

    PRATICA 01

---
+++ praticas/pratica01/atv-pratica01/components/despesa/DespesaItem.js
@@ -0,0 +1,45 @@
+import {View, Text, Pressable, StyleSheet} from 'react-native'
+
+function getDataFormatada(data){
+    return data.getDate() + '/' + (data.getMonth() + 1) + '/' +data.getFullYear();
+}
+
+function DespesaItem({item}){
+
+    return (
+        <Pressable>
+            <View style={styles.itemContainer}>
+                <View style={StyleSheet.itemText}>
+                    <Text>{getDataFormatada(item.data)}</Text>
+                </View>
+                <View style={styles.itemText}>
+                    <Text>(item.descicao)</Text>
+                </View>
+                <View style={styles.itemText}>
+                    <Text>(item.valor)</Text>
+                </View>
+
+            </View>
+        </Pressable>
+    );
+}
+
+const styles = StyleSheet.create({
+    itemContainer: {
+        flex: 1,
+        padding: 5,
+        marginVertical: 5,
+        marginHorizontal: 5,
+        backgroundColor: 'lightgray',
+        flexDirection: 'row',
+    },
+    itemText:{
+        flex: 1,
+        padding: 2,
+        marginVertical: 2,
+        marginHorizontal: 2,
+        alignContent: 'left',
+    },
+})
+
+export default DespesaItem
